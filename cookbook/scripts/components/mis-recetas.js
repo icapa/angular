29ae41a2fe1a858aa7ad323679ee32ff37592f71
icapa <ivan.olea@gmail.com>
2016-06-06 21:08:05 +0200

    Creado n componentes

---
+++ cookbook/scripts/components/mis-recetas.js
@@ -0,0 +1,18 @@
+angular
+    .module("cookbook")
+    .component("misRecetas",{
+        // Con 'templateUrl' establecemos la vista del componentes
+        templateUrl: "views/mis-recetas.html",
+        // En controlador establecemos la logica
+        controller: function(ServicioRecetas){
+            // Como 'obtenerRecetas()' retorna una promesa hay que pasar un manejador a su
+            // funcion 'then()'.
+            // Cercada para que funcione el this
+            var self = this;
+            ServicioRecetas.obtenerRecetas().then(function(respuesta) {
+                // En la pripiedad 'dada' de la respuesta http tenemos el cuerpo de la misma
+                self.recetas = respuesta.data;
+            });
+        }
+
+    });
--- cookbook/scripts/controllers/lista-recetas.js
+++ cookbook/scripts/controllers/lista-recetas.js
@@ -1,13 +1,7 @@
 angular
     .module("cookbook")
     .controller("ListaRecetas",function ($scope, ServicioRecetas) {
-        // Como 'obtenerRecetas()' retorna una promesa hay que pasar un manejador a su
-        // funcion 'then()'.
-        ServicioRecetas.obtenerRecetas()
-            .then(function(respuesta) {
-                // En la pripiedad 'dada' de la respuesta http tenemos el cuerpo de la misma
-                $scope.recetas = respuesta.data;
-            });
+        
         // Guardamos la receta
         
         $scope.guardarReceta = function(texto){
